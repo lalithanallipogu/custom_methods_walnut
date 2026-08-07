a448bfb977bfb06698ea654569199505f2adb416
Nallipogu Lalitha <lalitha.n@simplify3x.com>
2026-08-07 16:46:54 +0530

    Update custom methods

---
+++ walnut-methods/example.ts
@@ -0,0 +1,17 @@
+import type { WalnutContext } from './walnut';
+
+/** @walnut_method
+ * name: Login to Application
+ * description: Login with ${username} and ${password}
+ * actionType: custom_login
+ * context: web
+ * needsLocator: false
+ * category: Authentication
+ */
+export async function login(ctx: WalnutContext) {
+  await ctx.navigate(ctx.testBaseUrl + '/login');
+  await ctx.type('[data-testid="username"]', ctx.args[0]);
+  await ctx.type('[data-testid="password"]', ctx.args[1]);
+  await ctx.click('[data-testid="submit"]');
+  await ctx.verifyTextVisible('Dashboard');
+}
