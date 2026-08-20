c1bc04a7b9bc970e5f16b0bf427f46999d888434
Nallipogu Lalitha <lalitha.n@simplify3x.com>
2026-08-20 14:10:17 +0530

    Use ctx.args for SFTP credentials (same pattern as working generate-icmem.ts)

--- walnut-methods/replace-1-mem-50-claims-files-with-dummy-data-upload.ts
+++ walnut-methods/replace-1-mem-50-claims-files-with-dummy-data-upload.ts
@@ -5,7 +5,7 @@ import { spawnSync } from 'child_process';
 
 /** @walnut_method
  * name: Replace Files With Dummy Data and Upload
- * description: Read 4 files ${filePath1} ${filePath2} ${filePath3} ${filePath4}, replace {{member_id}} with ${dummyId} in temp copies, and upload to /To_AVER/ via SFTP
+ * description: Read 4 files ${filePath1} ${filePath2} ${filePath3} ${filePath4}, replace {{member_id}} with ${dummyId} in temp copies, and upload to /To_AVER/ via SFTP host ${sftpHost} port ${sftpPort} user ${sftpUsername} password ${sftpPassword}
  * actionType: custom_replace_files_with_dummy_data
  * context: shared
  * needsLocator: false
@@ -20,10 +20,10 @@ export async function replaceFilesWithDummyData(ctx: WalnutContext) {
 
   const filePaths = [ctx.args[0], ctx.args[1], ctx.args[2], ctx.args[3]];
   const dummyId = ctx.args[4];
-  const host = ctx.params.sftpHost;
-  const port = ctx.params.sftpPort || '22';
-  const username = ctx.params.sftpUsername;
-  const password = ctx.params.sftpPassword;
+  const host = ctx.args[5];
+  const port = ctx.args[6] || '22';
+  const username = ctx.args[7];
+  const password = ctx.args[8];
   const remoteDirectory = '/To_AVER/';
 
   if (!dummyId) {
